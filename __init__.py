24b5d1057d142de1b9f09ff31b181c43b60bca74
cfl <351382691@qq.com>
2025-02-20 17:12:49 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from .Inspyrenet_Rembg import InspyrenetRembg, InspyrenetRembgAdvanced
 
 NODE_CLASS_MAPPINGS = {
-    "InspyrenetRembg" : InspyrenetRembg,
-    "InspyrenetRembgAdvanced" : InspyrenetRembgAdvanced,
+    "InspyrenetRembgOpt" : InspyrenetRembg,
+    "InspyrenetRembgAdvancedOpt" : InspyrenetRembgAdvanced,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "InspyrenetRembg": "Inspyrenet Rembg",
-    "InspyrenetRembgAdvanced": "Inspyrenet Rembg Advanced"
+    "InspyrenetRembgOpt": "Inspyrenet Rembg Opt",
+    "InspyrenetRembgAdvancedOpt": "Inspyrenet Rembg Advanced Opt"
 }
 __all__ = ['NODE_CLASS_MAPPINGS', "NODE_DISPLAY_NAME_MAPPINGS"]
